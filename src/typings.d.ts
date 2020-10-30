1b12f67f6bdf1e8102dc24cfba9e084a90c31f47
Guillaume <hi@255kb.dev>
2020-10-30 08:06:38 +0100

    wip

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -3,3 +3,15 @@ declare var module: NodeModule;
 interface NodeModule {
   id: string;
 }
+
+// extend Express' Request type
+declare namespace Express {
+  export interface Request {
+    proxied: boolean;
+  }
+  export interface Response {
+    body: any;
+    routeUUID: string;
+    routeResponseUUID: string;
+  }
+}
